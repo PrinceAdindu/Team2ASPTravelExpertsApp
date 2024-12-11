a39712fc362e7b7a0fe94f603625491c92e98bf4
kazifattah <kazifattah52@gmail.com>
2024-12-11 14:52:15 -0700

    - customer details, edit and order history pages added to TEMVC - bookingsmanager, customermanager, bookingsdto classes added to TEData

---
+++ TravelExpertsData/BookingsDTO.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelExpertsData
+{
+    public class BookingsDTO
+    {
+        [Display(Name ="Booking Description")]
+        public string BookingDescription { get; set; }
+
+        [Display(Name = "Destination")]
+        public string BookingDestination { get; set; }
+
+        [Display(Name = "Booking #")]
+        public string BookingNo { get; set; }
+
+        [Display(Name = "Trip Type")]
+        public string TripTypeName { get; set; }
+
+        [Display(Name = "Flight Class")]
+        public string ClassName { get; set; }
+
+        [Display(Name = "Region")]
+        public string RegionName { get; set; }
+
+        [Display(Name = "Fee Amount")]
+        public decimal FeeAmount { get; set; }
+
+        [Display(Name = "Fee Details")]
+        public string FeeName { get; set; }
+
+        [Display(Name ="Base Price")]
+        public decimal? BasePrice { get; set; }
+
+        [Display(Name = "Agency Commission")]
+        public decimal? AgencyCommission { get; set; }
+    }
+}
---
+++ TravelExpertsData/CustomerManager.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelExpertsData
+{
+    public class CustomerManager
+    {
+        public static List<Customer> GetCustomers()
+        {
+            using (TravelExpertsContext db = new TravelExpertsContext())
+            {
+                List<Customer> customers = new List<Customer>();
+                customers = db.Customers.ToList();
+                return customers;
+            }
+            
+        }
+
+        public static Customer GetCustomerById(int id)
+        {
+            using (TravelExpertsContext db = new TravelExpertsContext())
+            {
+                Customer? customer = db.Customers.Find(id);
+                return customer;
+            }
+
+        }
+
+        public static void UpdateCustomer(int id, Customer customer)
+        {
+            using (TravelExpertsContext db = new TravelExpertsContext())
+            {
+                Customer? customerToEdit = db.Customers.Find(id);
+                if ( customerToEdit!=null)
+                {
+                    customerToEdit.CustFirstName = customer.CustFirstName;
+                    customerToEdit.CustLastName = customer.CustLastName;
+                    customerToEdit.CustAddress = customer.CustAddress;
+                    customerToEdit.CustCity = customer.CustCity;
+                    customerToEdit.CustProv = customer.CustProv;
+                    customerToEdit.CustPostal = customer.CustPostal;
+                    customerToEdit.CustCountry = customer.CustCountry;
+                    customerToEdit.CustHomePhone = customer.CustHomePhone;
+                    customerToEdit.CustBusPhone= customer.CustBusPhone;
+                    customerToEdit.CustEmail = customer.CustEmail;
+
+                }
+                db.SaveChanges();
+                
+
+            }
+        }
+    }
+}
