2d5c763b778fb420c8b5cf65592eeb74f8fff239
Prince Adindu <princcy4real@yahoo.com>
2024-12-11 16:40:23 -0700

    Packages view modification

--- TravelExpertsMVC/Controllers/PackageController.cs
+++ TravelExpertsMVC/Controllers/PackageController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using TravelExpertsData;
 
 namespace TravelExpertsMVC.Controllers
 {
     public class PackageController : Controller
     {
+        private readonly TravelExpertsContext _context;
+
+        public PackageController(TravelExpertsContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var package = _context.Packages.ToList();
+            return View(package);
         }
     }
 }
