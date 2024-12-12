0bcaac47cd27da5583e9fb5f4e95c23fca3a25ee
RyanOOSD <ryan.angaangan@edu.sait.ca>
2024-12-12 00:20:35 -0700

    Customer can now top-up balance with credit card

--- TravelExpertsMVC/Controllers/CustomerController.cs
+++ TravelExpertsMVC/Controllers/CustomerController.cs
@@ -7,7 +7,6 @@ namespace TravelExpertsMVC.Controllers
 {
     public class CustomerController : Controller
     {
-        // GET: CustomerController
         public ActionResult AddBalance()
         {
             Customer customer = CustomerManager.GetCustomerByID(104);
@@ -17,85 +16,44 @@ namespace TravelExpertsMVC.Controllers
             List<CreditCard> creditCards = CreditCardManager.GetCreditCardsByCustomer(id);
             if (creditCards != null)
             {
-                var list = new SelectList(creditCards, "CreditCardId", "CCNumber").ToList();
-                TempData["Error"] = "You do not have any saved credit cards, please add a card before continuing.";
+                var list = new SelectList(creditCards, "CreditCardId", "Ccnumber").ToList();
                 ViewBag.Cards = list;
             }
             else
             {
                 var list = new SelectList("").ToList();
+                TempData["NoCards"] = "You do not have any saved credit cards, please add a card before continuing.";
                 ViewBag.Cards = list;
             }
 
-            return View(customer);
+            CustomerDTO customerAddBalance = new CustomerDTO()
+            {
+                CurrentBalance = customer.Balance,
+                AddToBalance = 0,
+                CustCard = ""
+            };
+
+            return View(customerAddBalance);
         }
 
-        // GET: CustomerController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: CustomerController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: CustomerController/Create
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult AddBalance(CustomerDTO customerAddBalance)
         {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+            Customer customer = CustomerManager.GetCustomerByID(104);
+            List<CreditCard> creditCards = CreditCardManager.GetCreditCardsByCustomer(customer.CustomerId);
+            var list = new SelectList(creditCards, "CreditCardId", "Ccnumber").ToList();
+            ViewBag.Cards = list;
 
-        // GET: CustomerController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
+            if (ModelState.IsValid)
+            {
+                decimal updatedBalance = customerAddBalance.CurrentBalance + customerAddBalance.AddToBalance;
+                CustomerManager.UpdateBalance(customer.CustomerId, updatedBalance);
 
-        // POST: CustomerController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction("Index", "Package");
             }
-            catch
+            else
             {
-                return View();
-            }
-        }
-
-        // GET: CustomerController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: CustomerController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
+                return View(customerAddBalance);
             }
         }
     }
