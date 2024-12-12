f735ca5f4863dc1976c2a4e3f585cbdf58a87c07
RyanOOSD <ryan.angaangan@edu.sait.ca>
2024-12-12 02:20:19 -0700

    Customer can now add credit cards

--- TravelExpertsData/CreditCard.cs
+++ TravelExpertsData/CreditCard.cs
@@ -14,18 +14,23 @@ public partial class CreditCard
 
     [Column("CCName")]
     [StringLength(10)]
+    [Display(Name = "Card Type")]
     public string Ccname { get; set; } = null!;
 
     [Column("CCNumber")]
     [StringLength(50)]
+    [Display(Name = "Card Number")]
+    [RegularExpression("^([0-9]{4})([0-9]{4})([0-9]{4})([0-9]{4})$", ErrorMessage = "Ensure your card number is entered in the format 1111222233334444.")]
     public string Ccnumber { get; set; } = null!;
 
     [Column("CCExpiry", TypeName = "datetime")]
+    [Display(Name = "Expiry Date")]
+    [Range(typeof(DateTime), "2024-12-01", "2034-12-01", ErrorMessage = "Please ensure your card is not already expired.")]
     public DateTime Ccexpiry { get; set; }
 
     public int CustomerId { get; set; }
 
     [ForeignKey("CustomerId")]
     [InverseProperty("CreditCards")]
-    public virtual Customer Customer { get; set; } = null!;
+    public virtual Customer? Customer { get; set; } = null!;
 }
